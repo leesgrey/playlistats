97e7fa37facd33c8af128c7c262ee49374a0804c
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2020-06-23 19:50:21 -0400

    Started authentication tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+// tutorial by Joe Karlsson
+import React, { Component } from 'react';
+import * as $ from "jquery";
+import Player from "./Player";
+import hash from "./hash";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const clientId = "308136625304484d92879d69e98ccd89";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing";
+  "user-read-playback-state";
+]
+
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+window.location.hash = "";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images
+        }
+      }
+    }
+  }
+
+  componentDidMount() {
+    let _token = hash.access_token;
+    if (_token) {
+      this.setState({
+        token: _token;
+      });
+    }
+  }
+
+}
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!this.state.token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scopes.join("%20")}&response_type=token&show_dialog=true`}
+            >
+            Login to Spotify
+          </a>
+        )}
+        {this.state.token && (
+          //spotify player goes here
+        )}
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
