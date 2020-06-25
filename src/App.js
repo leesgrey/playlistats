eccf9c800e36933801ae3538d9132174745970b5
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2020-06-25 17:02:25 -0400

    Completed currently playing tutorial

--- src/App.js
+++ src/App.js
@@ -11,22 +11,10 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 const clientId = "308136625304484d92879d69e98ccd89";
 const redirectUri = "http://localhost:3000";
 const scopes = [
-  "user-read-currently-playing";
-  "user-read-playback-state";
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state"
 ]
-
-const hash = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function(initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-
-window.location.hash = "";
 
 class App extends Component {
   constructor() {
@@ -35,42 +23,87 @@ class App extends Component {
       token: null,
       item: {
         album: {
-          images
-        }
-      }
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: ""}],
+        duration_ms: 0,
+      },
+      is_playing: "Paused",
+      progress_ms: 0,
+      no_data: false
     }
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+    this.tick = this.tick.bind(this);
   }
 
   componentDidMount() {
     let _token = hash.access_token;
     if (_token) {
       this.setState({
-        token: _token;
+        token: _token
       });
+    }
+
+    this.tick();
+    // poll every 5 seconds
+    this.interval = setInterval(() => this.tick(), 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    if (this.state.token) {
+      this.getCurrentlyPlaying(this.state.token);
     }
   }
 
-}
+  getCurrentlyPlaying(token) {
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization" , "Bearer " + token);
+      },
+      success: (data) => {
+        if (!data) {
+          this.setState({
+            no_data: true
+          });
+          return;
+        }
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+          no_data: false
+        });
+      }
+    });
+  }
 
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {!this.state.token && (
-          <a
-            className="btn btn--loginApp-link"
-            href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scopes.join("%20")}&response_type=token&show_dialog=true`}
-            >
-            Login to Spotify
-          </a>
-        )}
-        {this.state.token && (
-          //spotify player goes here
-        )}
-      </header>
-    </div>
-  )
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+              >
+              Login to Spotify
+            </a>
+          )}
+          {this.state.token && (
+            <Player item={this.state.item} is_playing={this.state.is_playing} progress_ms={this.progress_ms}/>
+          )}
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
