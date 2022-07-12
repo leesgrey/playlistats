65e2a58e45b14eb7156faf3a91d9771e741136cc
leesgrey <sieun.v.lee@gmail.com>
2022-07-12 02:53:38 -0400

    Updates

---
+++ precache-manifest.882ee6fe0c421b7adde3e05cb81954d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144a567e2e2c27f0e2bd150754c33bdb",
+    "url": "/playlistats/index.html"
+  },
+  {
+    "revision": "3181258b2c46437099d0",
+    "url": "/playlistats/static/css/main.8679bf9a.chunk.css"
+  },
+  {
+    "revision": "79bd4b6574e1e17d8d51",
+    "url": "/playlistats/static/js/2.9b8b3bd3.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/playlistats/static/js/2.9b8b3bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3181258b2c46437099d0",
+    "url": "/playlistats/static/js/main.70426c80.chunk.js"
+  },
+  {
+    "revision": "0226f1b7769043150b98",
+    "url": "/playlistats/static/js/runtime-main.277486b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/playlistats/precache-manifest.5e5d254eab5e7830eeafc92dfeaa0571.js"
+  "/playlistats/precache-manifest.882ee6fe0c421b7adde3e05cb81954d6.js"
 );
 
 self.addEventListener('message', (event) => {
