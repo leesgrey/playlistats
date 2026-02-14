b763b6051e6b174649b9f791c1dd7cf1f0ab37ed
Grey Lee <greysv.lee@gmail.com>
2026-02-13 23:48:29 -0500

    change to real redirect uri

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const PAGE_SIZE = 50;
 export const clientId = "308136625304484d92879d69e98ccd89";
-//export const redirectUri = "https://leesgrey.github.io/playlistats";
-export const redirectUri = "http://127.0.0.1:5173";
+export const redirectUri = "https://leesgrey.github.io/playlistats";
+// export const redirectUri = "http://127.0.0.1:5173";
