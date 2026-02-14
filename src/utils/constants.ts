05bb613dba3a70ca7a50ecf78dc99ee26b8dcde7
Grey Lee <greysv.lee@gmail.com>
2026-02-14 00:13:52 -0500

    add envs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,3 @@
 export const PAGE_SIZE = 50;
-export const clientId = "308136625304484d92879d69e98ccd89";
-export const redirectUri = "https://leesgrey.github.io/playlistats";
-// export const redirectUri = "http://127.0.0.1:5173";
+export const clientId = import.meta.env.VITE_CLIENT_ID;
+export const redirectUri = import.meta.env.VITE_REDIRECT_URI;
